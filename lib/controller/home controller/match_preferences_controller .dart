40e69f1400e4dd752ad6f4909b982c47f4f5d653
Syed-Muhammad-Ali <syedmuhammadali0308@gmail.com>
2025-10-08 22:28:38 +0500

    new changs

--- lib/controller/home controller/match_preferences_controller .dart
+++ lib/controller/home controller/match_preferences_controller .dart
@@ -1,55 +1,55 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
+// import 'package:flutter/material.dart';
+// import 'package:get/get.dart';
 
-class MatchPreferencesController extends GetxController {
-  RxInt selectedTab = 0.obs;
-  
-  void selectTab(int index) {
-    selectedTab.value = index;
-  }
+// class MatchPreferencesController extends GetxController {
+//   RxInt selectedTab = 0.obs;
 
-  RxList<String> tabs =
-      [
-        'Match Preferences',
-        'Basic Details',
-        'Religious Details',
-        'Professional Details',
-        'Location Details',
-        'Family Details',
-      ].obs;
+//   void selectTab(int index) {
+//     selectedTab.value = index;
+//   }
 
-  // Match Preferences
-  RxMap<String, bool> matchFilters =
-      <String, bool>{
-        "All Matches": false,
-        "Newly Joined": false,
-        "Viewed You": false,
-        "Shortlisted You": false,
-        "Viewed By You": false,
-        "Shortlisted By You": false,
-        "Sent Request": false,
-        "Receive Request": false,
-        "Accepted Request": false,
-      }.obs;
+//   RxList<String> tabs =
+//       [
+//         // 'Match Preferences',
+//         'Basic Details',
+//         'Religious Details',
+//         'Professional Details',
+//         'Location Details',
+//         'Family Details',
+//       ].obs;
 
-  void toggleFilter(String key, bool? value) {
-    if (value != null) {
-      matchFilters[key] = value;
-    }
-  }
+//   // Match Preferences
+//   RxMap<String, bool> matchFilters =
+//       <String, bool>{
+//         "All Matches": false,
+//         "Newly Joined": false,
+//         "Viewed You": false,
+//         "Shortlisted You": false,
+//         "Viewed By You": false,
+//         "Shortlisted By You": false,
+//         "Sent Request": false,
+//         "Receive Request": false,
+//         "Accepted Request": false,
+//       }.obs;
 
-  // Basic Details
-  Rx<RangeValues> ageRange = RangeValues(18, 60).obs;
-  RxSet<String> selectedHeights = <String>{}.obs;
+//   void toggleFilter(String key, bool? value) {
+//     if (value != null) {
+//       matchFilters[key] = value;
+//     }
+//   }
 
-  void updateAgeRange(RangeValues values) {
-    ageRange.value = values;
-  }
+//   // Basic Details
+//   Rx<RangeValues> ageRange = RangeValues(18, 60).obs;
+//   RxSet<String> selectedHeights = <String>{}.obs;
 
-  void toggleSelection(String label) {
-    if (selectedHeights.contains(label)) {
-    } else {
-      selectedHeights.add(label);
-    }
-  }
-}
+//   void updateAgeRange(RangeValues values) {
+//     ageRange.value = values;
+//   }
+
+//   void toggleSelection(String label) {
+//     if (selectedHeights.contains(label)) {
+//     } else {
+//       selectedHeights.add(label);
+//     }
+//   }
+// }
