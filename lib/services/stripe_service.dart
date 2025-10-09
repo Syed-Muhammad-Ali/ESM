bcdaa4d62a0cb68b7a191248e7d923683603e476
Syed-Muhammad-Ali <syedmuhammadali0308@gmail.com>
2025-10-09 22:53:12 +0500

    new

--- lib/services/stripe_service.dart
+++ lib/services/stripe_service.dart
@@ -1,51 +1,5 @@
-// import 'dart:convert';
-
-// import 'package:flutter_stripe/flutter_stripe.dart';
-// import 'package:http/http.dart' as http;
-
-// class StripeService {
-//   /// Replace with your Stripe secret key on your backend (never hardcode in client)
-//   final String backendUrl =
-//       "https://console.cloud.google.com/iam-admin/iam?project=eourpean-single-muslilm";
-
-//   Future<bool> makePayment({
-//     required int amount,
-//     required String currency,
-//   }) async {
-//     try {
-//       final paymentIntent = await _createPaymentIntent(amount, currency);
-
-//       await Stripe.instance.initPaymentSheet(
-//         paymentSheetParameters: SetupPaymentSheetParameters(
-//           paymentIntentClientSecret: paymentIntent['client_secret'],
-//           merchantDisplayName: 'european_single_marriage',
-//         ),
-//       );
-
-//       await Stripe.instance.presentPaymentSheet();
-
-//       return true;
-//     } catch (e) {
-//       print("Stripe Payment Error: $e");
-//       return false;
-//     }
-//   }
-
-//   Future<Map<String, dynamic>> _createPaymentIntent(
-//     int amount,
-//     String currency,
-//   ) async {
-//     final response = await http.post(
-//       Uri.parse(backendUrl),
-//       headers: {'Content-Type': 'application/json'},
-//       body: jsonEncode({'amount': amount * 100, 'currency': currency}),
-//     );
-//     return jsonDecode(response.body);
-//   }
-// }
-
-
 import 'dart:convert';
+
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
 import 'package:http/http.dart' as http;
